df340a0c712f5fa22ecd213360088d0bac0fab09
Haris Khan <harisk2922@gmail.com>
2024-10-23 23:53:54 +0500

    Navbar in progress

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+    return (
+        <nav className="container">
+            <div className="logo">
+                <img src="/images/logo.png" alt="logo" />
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
